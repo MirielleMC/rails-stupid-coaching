06d15744d95291dd02e484e293cfb05e91499164
Pamela Matias <pamelamc23@gmail.com>
2024-08-26 16:01:59 -0500

    exercise2

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer'
+
 end
